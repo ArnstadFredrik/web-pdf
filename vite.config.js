ccee2133ef290d47ca8300f0514b7fd13379e32c
Fredrik Solås Arnstad <fredrikarnstad@MacBook-Air-Fredrik.local>
2025-11-26 13:20:09 +0100

    preview port

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 3030,
+    allowedHosts: ['.kirken.xyz'],
+  },
   server: {
     port: 3000,
     host: '0.0.0.0',
-    allowedHosts: ['.kirken.xyz']
+    allowedHosts: ['.kirken.xyz'],
   }
 })
