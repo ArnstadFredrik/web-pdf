7bd837101fe7192ffa2b20c14ea9f034907ac863
Fredrik Solås Arnstad <fredrikarnstad@MacBook-Air-Fredrik.local>
2025-11-30 15:28:56 +0100

    error warning options list

--- src/components/InputOption.jsx
+++ src/components/InputOption.jsx
@@ -2,7 +2,7 @@ const InputOption = ({onChange, checked, label, type, id}) => {
     return (
     <div className={type}>
         <input type={type} id={id} onChange={onChange} checked={checked}/>
-        <label htmlFor={id}>{label}</label>
+        <label htmlFor={id}>{label}<span className="italic"> Det er noe problem med valg...</span></label>
     </div>
     )
 }
